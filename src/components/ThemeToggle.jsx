fb328771741afb1ceef1ecb6d20eee59f34c48a8
Arpita Singh <arpitasingh@Arpitas-Laptop.local>
2025-07-01 22:56:24 +0530

    Set dark mode as default and updated theme logic

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,17 +5,19 @@ import {cn} from "@/lib/utils";
 export const ThemeToggle=()=>{
     const [isDarkMode, setIsDarkMode]=useState(false);
 
-    useEffect(()=>{
-        const storedTheme=localStorage.getItem("theme")
-        if(storedTheme=="dark"){
-            setIsDarkMode(true);
-            document.documentElement.classList.add("dark");
-        }
-        else{
-            localStorage.setItem("theme","light");
-            setIsDarkMode(false);
-        }
-    },[]);
+        useEffect(() => {
+            const storedTheme = localStorage.getItem("theme");
+
+            if (storedTheme === "dark" || storedTheme === null) {  
+                document.documentElement.classList.add("dark");
+                localStorage.setItem("theme", "dark");               
+                setIsDarkMode(true);
+            } else {
+                document.documentElement.classList.remove("dark");   
+                setIsDarkMode(false);
+            }
+        }, []);
+
 
     const toggleTheme=()=>{
         if(isDarkMode){
